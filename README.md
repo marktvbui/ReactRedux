17f707d8bbbe38db2c4e95e4759b6b367f04692a
Mark Bui <tvyybui@yahoo.com>
2017-09-18 23:26:27 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Simple React + Redux practice app
---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+  return [
+    { title: 'Javascript: The Good Parts' },
+    { title: 'Game of Thrones' },
+    { title: 'Harry Potter' },
+    { title: 'Iliad' }
+  ]
+}
